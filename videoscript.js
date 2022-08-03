c99e1780c6c8d698f28b61adf3e3a4eb3fb7a523
SparklingIsGood <marc.desperiez@orange.fr>
2022-08-04 00:57:29 +0200

    mobile fix

--- videoscript.js
+++ videoscript.js
@@ -4,5 +4,9 @@ var pusher = new Pusher('9f1b602df68c549601f4', {
 
   var channel = pusher.subscribe('sync-launch');
   channel.bind('start-show', function(data) {
+    if (isMobile && isSafari) {
+        this.myVideo.playsInline = true
+        this.myVideo.autoplay = true
+      }
       document.getElementById('video').play();
   });
