813792d4064dc010cf8a1e61ff24b92a1da0ac31
SparklingIsGood <marc.desperiez@orange.fr>
2022-08-04 20:14:19 +0200

    added no sleep

--- videoscript.js
+++ videoscript.js
@@ -9,7 +9,11 @@ channel.bind('start-show', function (data) {
     launch = true;
 });
 
+var noSleep = new NoSleep();
+
 document.querySelector('button').addEventListener('click', async () => {
+    noSleep.enable();
+
     var vid = document.querySelector('video');
     vid.play();
     vid.pause();
@@ -22,6 +26,8 @@ document.querySelector('button').addEventListener('click', async () => {
 
     await waitUntil(() => launch == true)
 
+    noSleep.disable();
+    
     document.getElementById('deviceIndex').style.display = "none";
     document.getElementById('beforeStart').style.border = "none";
     vid.style.display = "block";
